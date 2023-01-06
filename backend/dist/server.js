fe5e339ed423ff433f2dcfe6885d075391e13a3a
matheussf100 <matheussf300@gmail.com>
2023-01-06 19:08:20 -0300

    Dependencias e config para conexão Back/front

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -7,5 +7,5 @@ const app_1 = __importDefault(require("./app"));
 const database_1 = __importDefault(require("./database"));
 database_1.default.sync();
 console.log('Database running at 3306');
-app_1.default.listen(3000);
-console.log('Server running at 3000');
+app_1.default.listen(3001);
+console.log('Server running at 3001');
